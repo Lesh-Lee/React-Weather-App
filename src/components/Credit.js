67e028a593c8cddea502ef02471721c39c406164
Onalerona Pako <onapako90@gmail.com>
2024-10-23 17:19:40 +0200

    Final project

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.shecodes.io/graduates/132108-lebogang-lydia-rabotho"
+        > Lebogang Rabotho </a>, <a href="https://github.com/Lesh-Lee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
